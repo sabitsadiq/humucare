8e3923aabe7dc2101d53bc8628a3c3120e0bc541
sadiq <sabitsdq@gmail.com>
2024-07-03 05:14:22 +0100

    updated the repo

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,37 +1,31 @@
 document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
-    // Prevent default form submission (replaces link behavior)
     event.preventDefault();
 
-    // Get references to form elements
     var email = document.getElementById("email");
     var password = document.getElementById("password");
 
-    // Clear any previous error messages
     clearErrorMessages();
 
-    // Validation logic
     var isValid = true;
 
-    // Check if Email is empty or has invalid format
     if (!validateEmail(email.value)) {
       showError(email, "Please enter a valid email address");
       isValid = false;
     }
 
-    // Check if Password is empty
     if (password.value === "") {
       showError(password, "Password cannot be empty");
       isValid = false;
     }
 
-    // Simulate user authentication (replace with your actual logic)
-    // This is a placeholder; you'll likely need to interact with a server for real authentication
     const validCredentials =
       email.value === "sample@gmail.com" && password.value === "password";
 
-    // Redirect to course page if validation and authentication are successful
+    if (isValid && !validCredentials) {
+      showError(password, "Wrong credentials");
+    }
     if (isValid && validCredentials) {
       window.location.href = "takeCourse.html";
     }
@@ -44,24 +38,19 @@ function validateEmail(email) {
 }
 
 function showError(element, message) {
-  // Add error class to the element
   element.classList.add("error");
 
-  // Create a span element to display the error message
   const errorSpan = document.createElement("span");
   errorSpan.classList.add("error-message");
   errorSpan.textContent = message;
 
-  // Insert the error message after the element
   element.parentNode.insertBefore(errorSpan, element.nextSibling);
 }
 
 function clearErrorMessages() {
-  // Remove error class from all input elements
   const errorInputs = document.querySelectorAll(".error");
   errorInputs.forEach((input) => input.classList.remove("error"));
 
-  // Remove any existing error messages
   const errorMessages = document.querySelectorAll(".error-message");
   errorMessages.forEach((message) => message.remove());
 }
